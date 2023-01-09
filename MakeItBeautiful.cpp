ea7eff99a994c2a5897ca7ab77e7b8e278e36346
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2023-01-09 16:03:02 +0500

    Create MakeItBeautiful.cpp

---
+++ MakeItBeautiful.cpp
@@ -0,0 +1,81 @@
+/*
+An array a is called ugly if it contains at least one element which is equal to the sum of all elements before it. If the array is not ugly, it is beautiful.
+For example:
+the array [6,3,9,6] is ugly: the element 9 is equal to 6+3;
+the array [5,5,7] is ugly: the element 5 (the second one) is equal to 5;
+the array [8,4,10,14] is beautiful: 8≠0, 4≠8, 10≠8+4, 14≠8+4+10, so there is no element which is equal to the sum of all elements before it.
+You are given an array a such that 1≤a1≤a2≤⋯≤an≤100. You have to reorder the elements of a in such a way that the resulting array is beautiful.
+Note that you are not allowed to insert new elements or erase existing ones, you can only change the order of elements of a. 
+You are allowed to keep the array a unchanged, if it is beautiful.
+
+Input
+The first line contains one integer t (1≤t≤2000) — the number of test cases.
+Each test case consists of two lines. The first line contains one integer n (2≤n≤50). The second line contains n integers a1,a2,…,an (1≤a1≤a2≤⋯≤an≤100).
+
+Output
+For each test case, print the answer as follows:
+if it is impossible to reorder the elements of a in such a way that it becomes beautiful, print NO;
+otherwise, in the first line, print YES. In the second line, print n integers — any beautiful array which can be obtained from a by reordering its elements.
+If there are multiple such arrays, print any of them.
+
+*/
+#include <iostream>
+#include <vector>
+#include <set>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> v,v1,v2;
+        set<int> s;
+        int num;
+        for (int i = 0; i < n; i++) {
+            cin >> num;
+            v.push_back(num);
+        }
+       sort(v.begin(), v.end(),greater<int>());
+       for (int i = 0; i < n; i++) {
+           bool is_in = s.find(v[i]) != s.end();
+           if (!is_in) {
+               s.insert(v[i]);
+               v2.push_back(v[i]);
+           }
+           else {
+               v1.push_back(v[i]);
+           }
+       }
+       if (s.size() >= 2) {
+           cout << "YES\n";
+           sort(v2.begin(), v2.end(), greater<int>());
+           for (int i = 0; i < v2.size(); i++) {
+               cout << v2[i] << " ";
+           }
+           for (int i = 0; i < v1.size(); i++) {
+               cout << v1[i] << " ";
+           }
+
+       }
+       else {
+           cout << "NO";
+       }
+ 
+        
+        cout << "\n";
+
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
